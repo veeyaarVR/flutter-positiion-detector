dd14f21a8a14ccd587775d17fb4059025a2b6a03
Vignesh Marimuthu <vignesh.m@cumulations.com>
2024-07-09 12:15:54 +0530

    added camera and mlkit dependencies

--- android/app/src/main/kotlin/com/example/flutter_check_post/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_check_post/MainActivity.kt
@@ -10,7 +10,7 @@ import io.flutter.plugin.common.MethodChannel
 class MainActivity : FlutterActivity() {
     private val CHANNEL = "camera_channel"
     private var resultData : MethodChannel.Result? = null
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         val methodChannel =
             MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
